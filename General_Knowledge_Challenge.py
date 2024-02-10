3327981e4388a0fea32881d040230c3201a69709
Prudencerich <maryejiropraise@gmail.com>
2024-02-10 13:00:37 +0100

    general_knowledge quiz

---
+++ General_Knowledge_Challenge.py
@@ -0,0 +1,17 @@
+print("Welcome to my General knowledge quiz!")
+
+playing = input("Are you ready to ace this? ")
+
+if playing != "yes":
+    quit()
+    
+print("Yay! shall we begin:)")
+
+answer = input("What is the capital of France? ")
+if answer == "Paris":
+    print("Way to go champ!")
+else:
+    print("opps! that is incorrect.")
+
+    
+
